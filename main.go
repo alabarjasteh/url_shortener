a4d6f15aed8f7ddd4c9ca6152d2e4ea0ca3fd93d
alabarjasteh <amirala2011@gmail.com>
2022-03-13 19:18:40 +0330

    initial

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alabarjasteh/url-shortener/handler"
+	"github.com/alabarjasteh/url-shortener/store"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the URL Shortener API",
+		})
+	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note that store initialization happens here
+	store.InitializeStore()
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
+
+}
